4c12bf660745aeb2fa73b9af47722c9846dd00cf
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2025-07-01 05:54:29 +0530

    Added data setting functionality

--- storage.go
+++ storage.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"sync"
 	"time"
 )
 
 type Storage struct {
-	// mu       sync.Mutex
+	mu       sync.Mutex
 	data     map[string][]byte
 	expiry   map[string]time.Time
 	counters map[string]int64
@@ -18,4 +19,26 @@ func NewStorage() *Storage {
 		counters: make(map[string]int64),
 	}
 
+}
+
+func (s *Storage) Set(key, val []byte) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	keyStr := string(key)
+	s.data[keyStr] = val
+	delete(s.expiry, keyStr)
+
+	return nil
+}
+
+func (s *Storage) SetWithExpiry(key, val []byte, expiry time.Duration) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	keyStr := string(key)
+	s.data[keyStr] = val
+	s.expiry[keyStr] = time.Now().Add(expiry)
+
+	return nil
 }
